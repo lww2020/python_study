08ad65afcbac8778ca74676eb0a2e4c556bb4055
davie <13439017540@163.com>
2019-01-19 17:13:00 +0800

    add files

--- Python_studies/print_end.py
+++ Python_studies/print_end.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-print('a', end=' ')
-print('b', end=' ')
-print('c')
+#
+number = 23
+guess = int(input('Enter an integer: '))
+
+if guess == number:
+    # 新块从这里开始
+    print('Congratulations, you guessed it.')
+    print('but you do not win any prizes!')
+    # 新块从这里结束
+elif guess < number:
+    # 另一代码块
+    print('No, it is a little higher than that')
+    # 你可以在此做任何你希望在该代码块内进行的事情
+else:
+    print('No, it is a little lower than that')
+    # 你必须通过测试一个大于(>)设置数的数字来到这里。
+
+print('Done')
+# 这最后一行语句将在
+# if 语句执行完毕后执行。
--- Python_studies/test.py
+++ Python_studies/test.py
@@ -9,3 +9,5 @@ info="This is the first sentence. \
 This is the second sentence."
 print(info)
 """
+info1=r"Newlines are indicated by \n"
+print(info1)
