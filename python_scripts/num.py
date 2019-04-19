aa033f8a514c19f7ecac6f37c9f2a448e452bafe
davie <13439017540@163.com>
2019-04-19 08:50:32 +0800

    add files

---
+++ python_scripts/num.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+使用while循环输入 1 2 3 4 5 6     8 9 10
+"""
+count = 1
+while count < 11:
+    if count == 7:
+        pass
+    else:
+        print(count)
+    count += 1
+print('---------- Done ----------')
---
+++ python_scripts/pwd_inpt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+# 导入getpass模块
+import getpass
+
+# 提示用户输入密码
+pwd = getpass.getpass("请输入密码: ")
+
+# 打印输入的内容
+print("您输入的密码为: ",pwd)
